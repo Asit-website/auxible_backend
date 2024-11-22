b7022b29217e8871e68f8b023a61c4c150af7ef0
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-11-22 22:08:53 +0530

    done

--- models/Lead/Lead.js
+++ models/Lead/Lead.js
@@ -1,68 +1,56 @@
 import mongoose from "mongoose";
 
 const mySchema = new mongoose.Schema({
-    LeadOwner: {
+  LeadOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  budget: String,
+  leadType: String,
+  name: String,
+  Mobile: String,
+  Email: String,
+  date: {
+    type: String,
+  },
+
+  Street: String,
+  City: String,
+  State: String,
+  ZipCode: String,
+  Country: String,
+  image: String,
+  LeadSource: String,
+
+  isOpen: {
+    type: String,
+    default: "true",
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  status: {
+    type: String,
+    default: "Open",
+  },
+  closeDate: {
+    type: String,
+  },
+  invoiceId: [
+    {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User'
+      ref: "Invoice",
     },
-    Company: String,
-    FirstName: String,
-    LastName: String,
-    Title: String,
-    Email: String,
-    Phone: String,
-    Fax: String,
-    Mobile:  String,
-    Website:  String,
-    LeadSource:  String,
-    NoOfEmployee:  String,
-    Industry:  String,
-    LeadStatus:  String,
-    AnnualRevenue:  String,
-    Rating:  String,
-    EmailOptOut:  String,
-    SkypeID:  String,
-    SecondaryEmail:  String,
-    Twitter:  String,
-     Street:String ,
-     City:String ,
-     State:String ,
-     ZipCode:String ,
-     Country:String ,
-     DescriptionInfo:String ,
-     image:String , 
-     date:{
-      type: String, 
-     } , 
-     isOpen:{
-      type: String , 
-      default: "true" , 
-     } ,
-     createAt: {
-      type: Date , 
-      default:Date.now()
-     },
-     status:{
-      type:String,
-      default:"Open"
-     },
-     closeDate:{
-      type:String,
-     },
-          invoiceId:[
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Invoice'
-      }
-     ],
-     quatationId:[
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Quatation'
-      }
-     ]
-  });
+  ],
+  quatationId: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Quatation",
+    },
+  ],
+});
 
-  const Lead = mongoose.model("Lead", mySchema);
+const Lead = mongoose.model("Lead", mySchema);
 
-  export default Lead;
+export default Lead;
